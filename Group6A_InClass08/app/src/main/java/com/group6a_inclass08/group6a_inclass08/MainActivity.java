b1d8bca4ce3ac6323c952f486eed89973749a17d
Dismembered <mvitulli@uncc.edu>
2015-11-02 18:59:35 -0500

    Initial UI

--- Group6A_InClass08/app/src/main/java/com/group6a_inclass08/group6a_inclass08/MainActivity.java
+++ Group6A_InClass08/app/src/main/java/com/group6a_inclass08/group6a_inclass08/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,5 +34,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void signUpOnClick (View aView){
+
+    }
+
+    public void cancelOnClick(View aView){
+
     }
 }
