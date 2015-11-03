eafb041b7181560b68450ae05e7c0adc1dab05d6
Arunkumar <b.arun410@gmail.com>
2015-11-02 19:07:06 -0500

    Initial Parse

--- Group6A_InClass08/app/src/main/java/com/group6a_inclass08/group6a_inclass08/MainActivity.java
+++ Group6A_InClass08/app/src/main/java/com/group6a_inclass08/group6a_inclass08/MainActivity.java
@@ -2,15 +2,40 @@ package com.group6a_inclass08.group6a_inclass08;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.parse.Parse;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.SaveCallback;
+
 public class MainActivity extends AppCompatActivity {
+
+    ParseObject fParse;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize Parse
+        Parse.initialize(this, "3HaYVvf3qFMsKrCrh3AjmdfSm1J2LFtQ5ogCg94v", "CuvIH6vAsYF2Sp7zmRAKty12R1MQ2RbKFGBfeSuW");
+
+        fParse = new ParseObject("Login");
+        fParse.put("Name","Arun kumar");
+        fParse.put("Email","asdsand@gmail.com");
+        fParse.put("Password","djasjdk");
+        fParse.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e==null)
+                    Log.d("Msg","Signup Success");
+                else Log.d("Msg","Signup Failed");
+            }
+        });
     }
 
     @Override
