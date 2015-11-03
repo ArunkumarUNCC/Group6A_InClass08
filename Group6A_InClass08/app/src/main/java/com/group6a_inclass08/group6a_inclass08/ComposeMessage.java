310d64c8e861f800e62627ff28233c01eea8375d
Dismembered <mvitulli@uncc.edu>
2015-11-02 19:30:34 -0500

    UI Stuff

---
+++ Group6A_InClass08/app/src/main/java/com/group6a_inclass08/group6a_inclass08/ComposeMessage.java
@@ -0,0 +1,45 @@
+package com.group6a_inclass08.group6a_inclass08;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class ComposeMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_compose_message);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_compose_message, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessageOnClick (View aView){
+
+        //if text field is empty
+        Toast.makeText(this, "No text entered", Toast.LENGTH_SHORT).show();
+    }
+}
